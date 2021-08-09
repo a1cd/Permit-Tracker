70af8b4bbcf7ddf17f432c62cdd8833e9113f6e3
a1cd <71281043+a1cd@users.noreply.github.com>
2021-08-09 13:28:34 -0400

    Initial Commit

---
+++ Permit Tracker/Permit_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Permit_TrackerApp.swift
+//  Permit Tracker
+//
+//  Created by Everett Wilber on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Permit_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
