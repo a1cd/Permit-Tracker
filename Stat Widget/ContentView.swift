84960047463de70bf530acf6385ab313fdda4a7c
a1cd <71281043+a1cd@users.noreply.github.com>
2022-02-17 18:50:45 -0500

    Add Widget (incomplete)

---
+++ Stat Widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stat Widget
+//
+//  Created by Everett Wilber on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stat Widget/Stat_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stat_WidgetApp.swift
+//  Stat Widget
+//
+//  Created by Everett Wilber on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Stat_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
